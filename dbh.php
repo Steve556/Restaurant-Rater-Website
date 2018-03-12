9b7c6e2a8e5b7c4336760887c6785275d2feb311
Hussein Sakr <hsakr011@uottawa.ca>
2018-03-11 21:30:43 -0400

    Added correct version of dbh.php Added script.sql which makes the tables Added database.php whose purpose is to make the table of returned data

--- dbh.php
+++ dbh.php
@@ -1,7 +1,11 @@
 <?php
-
-$conn = mysqli_connect('localhost', 'root', '', 'phplogin');
-
-if (!$conn) {
-    die("Connection failed: ".mysqli_connect_error());    
-}
+	$host="localhost";
+	$user="postgres";
+	$password="123321";
+	$dbname = "Project";
+	$port = "5432";
+	$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password port=$port");
+	if(!$conn) {
+		die("Connection failed: ".mysqli_connect_error());
+	}
+?>
---
+++ script.sql
@@ -0,0 +1,57 @@
+Create SCHEMA php_project AUTHORIZATION postgres;
+
+set search_path="php_project";
+
+CREATE TABLE Rater
+(
+	userID SERIAL primary key,
+	emailAddress varchar(64),
+	firstName varchar(16),
+	lastName varchar(16),
+	joinDate DATE,
+	userType varchar(16),
+	reputation varchar(1),
+	password varchar(16),
+	username varchar(16)
+);
+
+CREATE TABLE Rating
+(
+	userID BIGINT NOT NULL,
+	ratingDate DATE,
+	price INT,
+	food varchar(1),
+	mood varchar(1),
+	staff varchar(1),
+	comments varchar(420),
+	restaurantID varchar(16),
+	Primary key(userID, ratingDate)
+);
+
+CREATE TABLE Restaurant
+(
+	restaurantID SERIAL PRIMARY KEY,
+	restaurantName varchar(16),
+	restaurantType varchar(16),
+	restaurantWebsite varchar(20)
+);
+
+CREATE TABLE MenuItem
+(
+	itemID SERIAL PRIMARY key,
+	itemName varchar(32),
+	itemType varchar(32),
+	itemCategory varchar(32),
+	itemDescription varchar(32),
+	itemPrice INT,
+	restaurantID BIGINT NOT NULL
+);
+
+CREATE TABLE RatingItem
+(
+	userID BIGINT NOT NULL,
+	ratingDate DATE,
+	itemID BIGINT NOT NULL,
+	rating varchar(1),
+	ratingComment varchar(420)
+);
